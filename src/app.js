ef49d3d28b141bf7e6ddacc58b24a1e285f8ec6f
David <daviddiazgomez370@gmail.com>
2025-12-02 10:26:07 -0500

    feat(ui): agregar lista y boton a la UI

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ export function suma(a, b) {
   return a + b;
 }
 
-export function agregarItem(texto) {
+function agregarItem(texto) {
   const lista = document.getElementById("lista");
   const li = document.createElement("li");
   li.textContent = texto;
   lista.appendChild(li);
 }
+
